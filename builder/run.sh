67ede48c32707f22a1c607d1c997b3df3653b334
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-08-04 07:54:45 +0100

    Rewrite builder/run.sh for new run.sh

--- builder/run.sh
+++ builder/run.sh
@@ -2,18 +2,26 @@
 #
 # Builds the container used by other projects
 
-cmd_test() {
-    true
-}
-cmd_build() {
-    ./Dockerfile.sh > Dockerfile
-    # to create an image locally: docker build --tag sleepdiaryproject/builder .
-}
-cmd_upgrade() {
-    curl --silent https://nodejs.org/dist/index.json \
-         | sed -ne '/"version"/ { s/{"version":"v\([0-9]*\).*/\1/p ; q }' \
-         > node-version.txt \
-         || exit 2
-}
+case "$1" in
 
-. root/build-sleepdiary.sh
+    cmd_test)
+      true
+      ;;
+
+    cmd_build)
+        ./Dockerfile.sh > Dockerfile
+        # to create an image locally: docker build --tag sleepdiaryproject/builder .
+        ;;
+
+    cmd_upgrade)
+        curl --silent https://nodejs.org/dist/index.json \
+             | sed -ne '/"version"/ { s/{"version":"v\([0-9]*\).*/\1/p ; q }' \
+             > node-version.txt \
+             || exit 2
+        ;;
+
+    *)
+        exit 2
+        ;;
+
+esac
