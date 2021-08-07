b45041f730205d95ace702793cf3e74c8af698c0
Andrew Sayers <sleepdiary@pileofstuff.org>
2021-08-07 19:16:57 +0100

    Add merge-and-push to /run.sh

--- run.sh
+++ run.sh
@@ -33,6 +33,63 @@ case "$1" in
         done
         ;;
 
+    merge-and-push)
+
+        set -v # verbose mode - print commands to stderr
+        set -e # exit if any of the commands below return non-zero
+
+        #
+        # Check if there are changes to commit
+        #
+
+        if ! git diff --quiet
+        then
+            git diff
+            echo "Please commit all changes"
+            exit 2
+        fi
+
+        #
+        # Check if there's anything to do
+        #
+
+        if ! git rev-list HEAD..origin/main | grep -q .
+        then
+            echo "'main' has already been merged - stopping"
+            exit 0
+        fi
+
+        #
+        # Merge changes from main
+        #
+
+        git merge --strategy-option=theirs --no-edit origin/main
+
+        #
+        # Run the build itself
+        #
+
+        for DIR in $SUBDIRECTORIES
+        do
+            cd "$DIR"
+            "./run.sh" build || exit 2
+            "./run.sh" test  || exit 2
+            cd - > /dev/null
+        done
+
+        #
+        # Add/commit/push changes
+        #
+
+        git add .
+        if git diff --quiet HEAD
+        then echo "No changes to commit"
+        else git commit -a -m "Build updates from main branch"
+        fi
+        git push
+
+        ;;
+
     *)
         exit 2
         ;;
